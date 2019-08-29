e35fdb375c0b6157b8359ad5bb7212fccacaa94d
Jason Ruan <jasonruan@pursuit.org>
2019-08-29 14:22:55 -0400

    Adds tab bar controller, three each of navigation controller-embedded table view controllers and detail view controllers, adds and connects a cocoa touch file for each and gives the cells and segues identifiers

---
+++ Intro to Unit Testing Lab/Detail View Controllers/StarWarsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StarWarsDetailViewController.swift
+//  Intro to Unit Testing Lab
+//
+//  Created by Jason Ruan on 8/29/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
