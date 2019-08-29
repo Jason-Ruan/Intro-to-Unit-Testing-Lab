40f50ca4baa2f21b98f01d5f0a25bd5ff0df52f2
Jason Ruan <jasonruan@pursuit.org>
2019-08-29 14:42:02 -0400

    Creates three models and deletes placeholder test funcs in tests file

---
+++ Intro to Unit Testing Lab/Models/StarWars.swift
@@ -0,0 +1,13 @@
+//
+//  Star Wars.swift
+//  Intro to Unit Testing Lab
+//
+//  Created by Jason Ruan on 8/29/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
+
+struct StarWars {
+    
+}
